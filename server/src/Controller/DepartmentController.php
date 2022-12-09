c3a7ec6529507f618f54221f1f1f2a0e6103edfa
Avo <tungtrananh6@gmail.com>
2022-12-10 05:41:04 +0700

    Re-organize Controllers and create View All Departments

--- server/src/Controller/DepartmentController.php
+++ server/src/Controller/DepartmentController.php
@@ -2,27 +2,72 @@
 
 namespace App\Controller;
 
+use App\Repository\DepartmentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 #[Route('/api/department', name: 'api_department_')]
 class DepartmentController extends AbstractController
 {
     #[Route('/', name: 'view_all')]
-    public function viewAllDepartments(): JsonResponse
+    //TODO: implement
+    public function viewAllDepartments(DepartmentRepository $repository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DepartmentController.php',
-        ]);
+        $departments = $repository->findAll();
+        //Custom DQL to get id and name of department
+        $myDepartments = $repository->listDept();
+        $data = [];
+        foreach ($departments as $department) {
+            $data[] = [
+                'id' => $department->getId(),
+                'name' => $department->getName(),
+                'doctors' => $department->getDoctors()
+            ];
+        }
+        return $this->json(
+            $data,
+            // $myDepartments,
+            Response::HTTP_OK,
+            [],
+            [
+                //Ignore the unwanted attributes
+                ObjectNormalizer::IGNORED_ATTRIBUTES => ['userId', 'department', 'appointments'],
+                //Handle Circular Reference Exception in Association Relationship
+                ObjectNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object) {
+                    return $object->getId();
+                }
+            ]
+        );
     }
 
     #[Route('/{departmentId}', name: 'view_detail')]
+    //TODO: implement
     public function viewDepartmentDetail($departmentId): JsonResponse
     {
-        $department = $this->repository->find($departmentId);
-        return $this->json([$department]);
+        return $this->json([]);
     }
 
+    #[Route('/create', name: 'create', methods: 'POST')]
+    //TODO: implement
+    public function createDepartment(): JsonResponse
+    {
+        return $this->json([]);
+    }
+
+    #[Route('/{departmentId}/update', name: 'update', methods: 'PUT')]
+    //TODO: implement
+    public function updateDepartment(): JsonResponse
+    {
+        return $this->json([]);
+    }
+
+    #[Route('/{departmentId}/delete', name: 'delete', methods: 'DELETE')]
+    //TODO: implement
+    public function deleteDepartment(): JsonResponse
+    {
+        return $this->json([]);
+    }
 }
