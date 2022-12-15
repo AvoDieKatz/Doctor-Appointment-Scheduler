837cfb78bcd16806b7821a8474e27a71a8db52c8
Avo <tungtrananh6@gmail.com>
2022-12-16 00:15:24 +0700

    Dev 12 - Create Add Appointment feature for actor Patient

--- server/src/Controller/AppointmentController.php
+++ server/src/Controller/AppointmentController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Appointment;
 use App\Repository\AppointmentRepository;
+use App\Repository\DepartmentRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -90,9 +93,54 @@ class AppointmentController extends AbstractController
     }
 
     #[Route('/create', name: 'create', methods: 'POST')]
-    //TODO: implement
-    public function createAppointment(): JsonResponse
+    public function createAppointment(DepartmentRepository $departmentRepository, Request $request): JsonResponse
     {
-        return $this->json([]);
+        // Create an empty Appointment object
+        $appointment = new Appointment;
+
+        // Decode the request sent, toArray() use json_encode internally
+        $data = $request->toArray();
+
+        // Find the chosen Department
+        $department = $departmentRepository->find($data['department_id']);
+
+        $doctor = null;
+
+        if ($department != null) {
+            // Get all the doctors belong to that department, this will return an array of
+            $department_doctors = $department->getDoctors();
+            // A loop to iterate all the doctors found in this Department and check if its ID match the ID from client's request
+            foreach ($department_doctors as $department_doctor) {
+                if ($department_doctor->getId() === $data['doctor_id']) {
+                    $doctor = $department_doctor;
+                }
+            }
+        }
+
+        // Because if the department is null, it will pass the previous 'if', the doctor variable will not be updated, therefore this 'if' will run
+        if ($doctor == null) {
+            return $this->json(
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        // If both Department and Doctor are not null, proceed to add to the Appointment object
+        $appointment->setPatientName($data['patient_name'])
+            ->setPatientDob(\DateTime::createFromFormat('m/d/Y', $data['dob']))
+            ->setPatientGender($data['gender'])
+            ->setPatientDepartment($department)
+            ->setDoctor($doctor)
+            ->setDate(\DateTime::createFromFormat('m/d/Y', $data['appointed_date']))
+            ->setPatientMessage($data['patient_message'])
+            ->setDone(false);
+
+        // Let doctrine create a new row in database
+        $this->manager->persist($appointment);
+        $this->manager->flush();
+
+        return $this->json(
+            ['message' => 'The appointment is succesfully created'],
+            Response::HTTP_OK,
+        );
     }
 }
