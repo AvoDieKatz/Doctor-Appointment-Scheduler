dd7f143b2ca744bbe33c4f93c1bf69cc303eb5ec
Avo <tungtrananh6@gmail.com>
2022-12-23 23:43:00 +0700

    Fix Header Account avatar to only show when user logged in

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,17 +1,36 @@
-import React from "react";
-import { Box, Button, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+    Box,
+    Toolbar,
+    Typography,
+    Tooltip,
+    Menu,
+    MenuItem,
+    Divider,
+    IconButton,
+    Avatar,
+    ListItemIcon,
+} from "@mui/material";
+import Logout from "@mui/icons-material/Logout";
 import { getUsername, removeUserSession } from "../utils/common";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-
     const username = getUsername();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     const handleLogout = () => {
-        removeUserSession()
-        navigate('/')
-    }
-    
+        removeUserSession();
+        navigate("/");
+    };
+
     return (
         <Box sx={{ flex: "0 1 auto" }} className="green-background">
             <Toolbar>
@@ -22,10 +41,77 @@ const Header = () => {
                 >
                     Doctor Portal
                 </Typography>
-                <Typography color="primary.text">
-                    You have logged in as {username}
-                </Typography>
-                <Button className="danger" onClick={handleLogout}>Log out</Button>
+
+                {username && (
+                    <>
+                        <Tooltip>
+                            <IconButton
+                                onClick={handleClick}
+                                size="small"
+                                sx={{ ml: 2 }}
+                                aria-controls={
+                                    open ? "account-menu" : undefined
+                                }
+                                aria-haspopup="true"
+                                aria-expanded={open ? "true" : undefined}
+                            >
+                                <Avatar sx={{ width: 32, height: 32 }}></Avatar>
+                            </IconButton>
+                        </Tooltip>
+
+                        <Menu
+                            anchorEl={anchorEl}
+                            id="account-menu"
+                            open={open}
+                            onClose={handleClose}
+                            onClick={handleClose}
+                            PaperProps={{
+                                elevation: 0,
+                                sx: {
+                                    overflow: "visible",
+                                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                                    mt: 1.5,
+                                    "& .MuiAvatar-root": {
+                                        width: 32,
+                                        height: 32,
+                                        ml: -0.5,
+                                        mr: 1,
+                                    },
+                                    "&:before": {
+                                        content: '""',
+                                        display: "block",
+                                        position: "absolute",
+                                        top: 0,
+                                        right: 14,
+                                        width: 10,
+                                        height: 10,
+                                        bgcolor: "background.paper",
+                                        transform:
+                                            "translateY(-50%) rotate(45deg)",
+                                        zIndex: 0,
+                                    },
+                                },
+                            }}
+                            transformOrigin={{
+                                horizontal: "right",
+                                vertical: "top",
+                            }}
+                            anchorOrigin={{
+                                horizontal: "right",
+                                vertical: "bottom",
+                            }}
+                        >
+                            <MenuItem>{username}</MenuItem>
+                            <Divider />
+                            <MenuItem onClick={handleLogout}>
+                                <ListItemIcon>
+                                    <Logout fontSize="small" />
+                                </ListItemIcon>
+                                Logout
+                            </MenuItem>
+                        </Menu>
+                    </>
+                )}
             </Toolbar>
         </Box>
     );
